a55ae45b86c708c4675ea2aa5c228d1aca07de92
Kavinda Manohara <kavindamanoharak@gmail.com>
2025-01-29 19:24:41 +0530

    card animation

---
+++ components/card_animation/cardanimation.tsx
@@ -0,0 +1,119 @@
+import React from 'react'
+import Image from "next/image";
+import Card01 from '../../assets/images/landing/card01.png'
+import '../card_animation/cardanimationstyle.css'
+import { Button } from "../ui/button";
+function cardanimation() {
+  return (
+    <div>
+        <div className='container mx-auto mb-[50px] mt-[100px]'>
+        <h1 className="title-text-stroke text-[40px] font-semibold leading-none sm:text-[50px] md:text-[60px] lg:text-[80px] xl:text-[95px] 2xl:text-[108px]">
+          Exceptional <br /> Quality
+        </h1>
+        <div className='bg-red-600 w-[200px] h-[5px] mt-[20px]'></div>
+        <div className='bg-[#D9D9D9] w-full h-[40px] mt-[30px] flex items-center '>
+            <p className='ml-[30px] font-semibold'>Effortless Warmth for Every Home</p>
+        </div>
+    </div>
+    <div className='landingcardssection relative bg-white'>
+      <div className='background w-[100%] h-[100vh] lg:h-[100vh] bg-white sticky top-0 '></div>
+
+      <div className='content container mx-auto mt-[-100vh]'>
+        <div className='cards'>
+            <div className='cards-grid'>
+<div className='card'>
+    <div className='card-inner bg-white border-t-4 border-black h-[1100px]  md:h-[1000px] lg:h-[500px] xl:h-[500px] 2xl:h-[700px] flex items-center'>
+    <div className='grid grid-cols-1 lg:grid-cols-2 container'>
+        <div className=''>
+        <Image
+                className="aboutUs h-[400px]  lg:h-[500px] xl:h-[500px] 2xl:h-[700px] w-auto p-[30px] lg:p-[50px]"
+                src={Card01}
+                alt="product"
+          />
+        </div>
+        <div className='p-[50px] lg:p-[50px] flex justify-center items-center '>
+            <div className=''>
+            <p>Main Categories</p>
+            <h1 className='text-[40px] sm:text-[60px] lg:text-[40px] xl:text-[50px] font-semibold leading-none'>Residential<br></br>Boilers</h1>
+            <div className="bg-red-600 w-[100px] sm:w-[200px] h-[2px] sm:h-[5px]"></div>
+            <br></br>
+            <p className='text-[20px] xl:text-[15px] 2xl:text-[20px] font-semibold xl:mt-[-20px]'>Effortless Warmth for Every Home</p>
+            <br></br>
+            <p className='text-[15px] sm:text-[25px] lg:text-[15px] xl:text-[15px] 2xl:sm:text-[25px] mt-[-20px] xl:mt-[0px] '>When users hover over the main categories, the subcategories should appear along with their respective category images. </p>
+            <ul className='ml-12 mt-6 text-[14px] sm:text-[20px] lg:text-[13px] xl:text-[13px] 2xl:text-[20px]'>
+                <li>Oil Fired Residential Boilers</li>
+                <li>Gas Fired Residential Boilers</li>
+            </ul>
+            <br></br>
+            <div className='flex items-center gap-7'>
+                <div>
+            <Button variant="primary">Explore Our Boilers</Button>
+                </div>
+                <div>
+            <p className="">Learn more</p>
+                </div>
+            </div>
+        </div>
+        </div>
+        
+    </div>
+    </div>
+</div>
+
+<div className='card'>
+    <div className='card-inner bg-white border-t-4 border-black h-[1100px]  md:h-[1000px] lg:h-[500px] xl:h-[500px] 2xl:h-[700px] flex items-center mt-[500px] sm:mt-[500px] lg:mt-[100px]'>
+    <div className='grid grid-cols-1 lg:grid-cols-2 container'>
+        <div className=''>
+        <Image
+                className="aboutUs h-[400px]  lg:h-[500px] xl:h-[500px] 2xl:h-[700px] w-auto p-[30px] lg:p-[50px]"
+                src={Card01}
+                alt="product"
+          />
+        </div>
+        <div className='p-[50px] lg:p-[50px] flex justify-center items-center '>
+            <div className=''>
+            <p>Main Categories</p>
+            <h1 className='text-[40px] sm:text-[60px] lg:text-[40px] xl:text-[50px] font-semibold leading-none'>Residential<br></br>Boilers</h1>
+            <div className="bg-red-600 w-[100px] sm:w-[200px] h-[2px] sm:h-[5px]"></div>
+            <br></br>
+            <p className='text-[20px] xl:text-[15px] 2xl:text-[20px] font-semibold xl:mt-[-20px]'>Effortless Warmth for Every Home</p>
+            <br></br>
+            <p className='text-[15px] sm:text-[25px] lg:text-[15px] xl:text-[15px] 2xl:sm:text-[25px] mt-[-20px] xl:mt-[0px] '>When users hover over the main categories, the subcategories should appear along with their respective category images. </p>
+            <ul className='ml-12 mt-6 text-[14px] sm:text-[20px] lg:text-[13px] xl:text-[13px] 2xl:text-[20px]'>
+                <li>Oil Fired Residential Boilers</li>
+                <li>Gas Fired Residential Boilers</li>
+            </ul>
+            <br></br>
+            <div className='flex items-center gap-7'>
+                <div>
+            <Button variant="primary">Explore Our Boilers</Button>
+                </div>
+                <div>
+            <p className="">Learn more</p>
+                </div>
+            </div>
+        </div>
+        </div>
+        
+    </div>
+    </div>
+</div>
+
+
+            </div>
+        </div>
+      </div>
+     
+      </div>
+<div className='bg-white h-[100px] sm:h-[100px] md:h-[30px] lg:h-[300px] xl:h-[500px] 2xl;h-[100px]'></div>
+
+      {/* section 02 */}
+
+
+      
+
+    </div>
+  )
+}
+
+export default cardanimation
--- components/landing-page/MainCategories.tsx
+++ components/landing-page/MainCategories.tsx
@@ -1,106 +1,18 @@
 import React from "react";
-import Image from "next/image";
 
-import { Button } from "../ui/button";
-import { RiFileListLine } from "react-icons/ri";
-import { FaSquare } from "react-icons/fa";
-import { HiArrowLongRight } from "react-icons/hi2";
 
-import boiler from "@/assets/images/landing-page/main-categories/boiler.png";
+import Cardanimation from "../card_animation/cardanimation";
 
-const categories = [
-  {
-    name: "Residential boilers",
-    slogan: "Effortless Warmth for Every Home",
-    boilerImage: boiler,
-    desc: "When users hover over the main categories, the subcategories should appear along with their respective category images.",
-    boilerNames: ["Combi", "Ultra"],
-  },
-  {
-    name: "Commercial boilers",
-    slogan: "Effortless Warmth for Every Home",
-    boilerImage: boiler,
-    desc: "When users hover over the main categories, the subcategories should appear along with their respective category images.",
-    boilerNames: ["Optimajor", "Commodore"],
-  },
-];
+
+
+
 
 const MainCategories = () => {
   return (
-    <section className="container mx-auto px-[15px] text-[14px] sm:text-[17px] md:text-[20px] lg:text-[22px] xl:text-[24px] 2xl:text-[27px]">
-      {categories.map(({ name, slogan, boilerImage, desc, boilerNames }) => (
-        <React.Fragment key={name}>
-          {/* Category name */}
-          <h1 className="title-text-stroke text-[48px] capitalize leading-[0.9em] sm:text-[76px] md:text-[104px] lg:text-[132px] xl:text-[160px] 2xl:text-[196px]">
-            {name.split(" ")[0]} <br />
-            {name.split(" ")[1]}
-          </h1>
-
-          {/* Slogan */}
-          <p className="mt-[0.4em] bg-[#D9D9D9] py-[0.2em] ps-[0.8em] font-semibold">
-            {slogan}
-          </p>
-
-          {/* Grid */}
-          <div className="mb-[3em] mt-[4.5em] grid justify-items-center gap-[1.3em] lg:grid-cols-2 lg:items-center lg:gap-0">
-            {/* Image area */}
-            <div className="relative w-[260px] text-[40px] sm:w-[310px] sm:text-[50px] md:w-[360px] md:text-[60px] lg:w-[420px] lg:text-[68px] xl:w-[481px] xl:text-[72px] 2xl:w-[530px] 2xl:text-[76px]">
-              <Image
-                src={boilerImage}
-                alt="boiler"
-                className="relative z-[1] object-contain"
-              />
-              <h2 className="title-text-stroke absolute -top-[6%] left-0 uppercase leading-none">
-                {boilerNames[0]}
-              </h2>
-            </div>
-
-            {/* Text area */}
-            <div>
-              <h4 className="flex items-center gap-[0.3em] text-[0.9em] font-medium capitalize text-black/50 md:text-[0.8em] lg:text-[0.7em] 2xl:text-[0.6em]">
-                <RiFileListLine className="-translate-y-[0.1em] text-[1.2em]" />
-                Main categories
-              </h4>
-              {/* Category name */}
-              <h3 className="mb-[0.2em] mt-[0.1em] text-[46px] font-bold capitalize leading-none sm:text-[54px] md:text-[62px] lg:text-[66px] xl:text-[68px] 2xl:text-[72px]">
-                {name.split(" ")[0]} <br />
-                {name.split(" ")[1]}
-                <hr className="w-[2.65ch] border-t-[6px] border-t-primary" />
-              </h3>
-
-              {/* Slogan */}
-              <p className="text-[1.2em] font-semibold leading-none">
-                {slogan}
-              </p>
-
-              {/* Desc */}
-              <p className="my-[0.5em] leading-tight md:text-[0.9em] lg:text-[0.8em] xl:text-[0.7em] 2xl:text-[0.6em]">
-                {desc}
-              </p>
-
-              {/* Types */}
-              <ul className="list-inside ps-[4ch] font-medium *:flex *:items-center *:gap-[0.3em]">
-                {boilerNames.map((name) => (
-                  <li key={name}>
-                    <FaSquare className="text-[0.5em]" />
-                    {name}
-                  </li>
-                ))}
-              </ul>
-
-              <div className="mt-[1.4em] flex flex-wrap items-center gap-[0.8em] sm:text-[13px] md:text-[14px] lg:text-[15px] xl:text-[16px] 2xl:text-[16px]">
-                <Button variant="primary" className="w-fit">
-                  Explore Our boilers
-                </Button>
-                <Button size="none" className="ms-[0.5em] w-fit">
-                  Learn more <HiArrowLongRight />
-                </Button>
-              </div>
-            </div>
-          </div>
-        </React.Fragment>
-      ))}
-    </section>
+    <div>
+      <Cardanimation />
+ 
+    </div>
   );
 };
 
