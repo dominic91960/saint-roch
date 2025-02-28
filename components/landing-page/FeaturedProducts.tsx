3f669b7382c03d835f8a9340cf561b63d6e96cf4
Dominic Brian <dominic91960@gmail.com>
2025-03-01 00:11:52 +0530

    feat(homepage): add product title to featured products

--- components/landing-page/FeaturedProducts.tsx
+++ components/landing-page/FeaturedProducts.tsx
@@ -25,36 +25,41 @@ const featuredProducts = [
     id: "01",
     category: "Boilers",
     images: [product, product, product, product],
+    title: "Product One",
     emphasis: "Display key",
-    desc: "certifications (e.g., ISO 9001, Energy Star, CE Marking) as clickable icons. On hover, provide a brief description of what the certification represents and how it",
+    desc: "certifications (e.g., ISO 9001, Energy Star, CE Marking) as clickable icons",
   },
   {
     id: "02",
     category: "Burners",
     images: [product, product, product, product],
+    title: "Product Two",
     emphasis: "Display key",
-    desc: "certifications (e.g., ISO 9001, Energy Star, CE Marking) as clickable icons. On hover, provide a brief description of what the certification represents and how it",
+    desc: "certifications (e.g., ISO 9001, Energy Star, CE Marking) as clickable icons",
   },
   {
     id: "03",
     category: "Hot water Tanks",
     images: [product, product, product, product],
+    title: "Product Three",
     emphasis: "Display key",
-    desc: "certifications (e.g., ISO 9001, Energy Star, CE Marking) as clickable icons. On hover, provide a brief description of what the certification represents and how it",
+    desc: "certifications (e.g., ISO 9001, Energy Star, CE Marking) as clickable icons",
   },
   {
     id: "04",
     category: "Pumps",
     images: [product, product, product, product],
+    title: "Product Four",
     emphasis: "Display key",
-    desc: "certifications (e.g., ISO 9001, Energy Star, CE Marking) as clickable icons. On hover, provide a brief description of what the certification represents and how it",
+    desc: "certifications (e.g., ISO 9001, Energy Star, CE Marking) as clickable icons",
   },
   {
     id: "05",
     category: "Heat pumps",
     images: [product, product, product, product],
+    title: "Product Five",
     emphasis: "Display key",
-    desc: "certifications (e.g., ISO 9001, Energy Star, CE Marking) as clickable icons. On hover, provide a brief description of what the certification represents and how it",
+    desc: "certifications (e.g., ISO 9001, Energy Star, CE Marking) as clickable icons",
   },
 ];
 
@@ -81,13 +86,13 @@ const SwiperNavButtons = () => {
 
 const FeaturedProducts = () => {
   return (
-    <section className="bg-[rgb(240,240,240)] text-[12px] sm:text-[15px] md:text-[18px] lg:text-[21px] xl:text-[24px] 2xl:text-[24px] mb-[4em]">
+    <section className="mb-[4em] bg-[rgb(240,240,240)] text-[12px] sm:text-[15px] md:text-[18px] lg:text-[21px] xl:text-[24px] 2xl:text-[24px]">
       <motion.div
         variants={SlideLeft(0.5)}
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true }}
-        className="section-padding container mx-auto pt-[2em] pb-[2em]"
+        className="section-padding container mx-auto pb-[2em] pt-[2em]"
       >
         <h1 className="mb-[0.4em] text-center text-[22px] font-semibold capitalize leading-tight sm:mb-[0.5em] sm:text-[29px] md:mb-[0.6em] md:text-[36px] lg:mb-[0.7em] lg:text-[44px] xl:mb-[0.8em] xl:text-[52px] 2xl:mb-[0.8em] 2xl:text-[60px]">
           Our featured products
@@ -110,70 +115,76 @@ const FeaturedProducts = () => {
           }}
           className="mySwiper"
         >
-          {featuredProducts.map(({ id, category, images, emphasis, desc }) => (
-            <SwiperSlide key={id}>
-              <div className="m-[10px] pb-[60px]">
-                <article
-                  key={id}
-                  className="relative w-full rounded-lg bg-white pb-[1.4em] pt-[2em] shadow-[1px_1px_5px_rgba(0,0,0,0.3)]"
-                >
-                  {/* Title */}
-                  <h2 className="w-fit bg-[#D9D9D9] pe-[1.7em] ps-[0.7em] text-[20px] font-bold capitalize leading-[2em] sm:text-[23px] md:text-[26px] lg:text-[30px] xl:text-[35px] 2xl:text-[40px]">
-                    {category}
-                  </h2>
-
-                  {/* Image with number*/}
-
-                  <Swiper
-                    modules={[Autoplay, EffectCreative, Navigation]}
-                    grabCursor
-                    effect={"creative"}
-                    creativeEffect={{
-                      prev: {
-                        translate: [0, 0, -400],
-                        opacity: 0,
-                      },
-                      next: {
-                        translate: ["100%", 0, 0],
-                      },
-                    }}
-                    speed={2000}
-                    loop={true}
-                    spaceBetween={10}
-                    slidesPerView={1}
-                    autoplay={{
-                      delay: 5000,
-                      // reverseDirection: true,
-                      disableOnInteraction: false,
-                      pauseOnMouseEnter: false,
-                    }}
-                    className="mySwiper -mt-[13%]"
+          {featuredProducts.map(
+            ({ id, category, images, title, emphasis, desc }) => (
+              <SwiperSlide key={id}>
+                <div className="m-[10px] pb-[60px]">
+                  <article
+                    key={id}
+                    className="relative w-full rounded-lg bg-white pb-[1.4em] pt-[2em] shadow-[1px_1px_5px_rgba(0,0,0,0.3)]"
                   >
-                    {images.map((image, i) => (
-                      <SwiperSlide key={i}>
-                        <div className="relative mx-auto mt-[8%] w-[232px] sm:w-[288px] md:w-[344px] lg:w-[400px] xl:w-[455px] 2xl:w-[512px]">
-                          <Image
-                            src={image}
-                            alt={category}
-                            className="relative z-[1] object-contain"
-                          />
-                        </div>
-                      </SwiperSlide>
-                    ))}
-                    <SwiperNavButtons />
-                  </Swiper>
+                    {/* Title */}
+                    <h2 className="w-fit bg-[#D9D9D9] pe-[1.7em] ps-[0.7em] text-[20px] font-bold capitalize leading-[2em] sm:text-[23px] md:text-[26px] lg:text-[30px] xl:text-[35px] 2xl:text-[40px]">
+                      {category}
+                    </h2>
 
-                  {/* Desc */}
-                  <p className="relative -mt-[2em] me-[1.2em] ms-auto max-w-[38ch] border-e-[0.4em] border-e-primary pe-[0.4em] pt-[2em] text-right text-[11px] leading-tight sm:me-[1.4em] sm:text-[12px] md:me-[1.6em] md:max-w-[46ch] md:text-[12.5px] lg:me-[1.8em] lg:text-[13px] xl:me-[2em] xl:text-[13.5px] 2xl:me-[2em] 2xl:border-e-[0.6em] 2xl:text-[14px]">
-                    <span className="absolute bottom-[60%] right-[20%] text-[60px] font-bold leading-none text-black/20 sm:text-[78px] md:text-[96px] lg:text-[114px] xl:text-[132px] 2xl:text-[150px]">
-                      {id}
-                    </span>
-                    <span className="font-bold">{emphasis}</span> {desc}
-                  </p>
-                </article>
-              </div>
-            </SwiperSlide>
-          ))}
+                    {/* Image with number*/}
+
+                    <Swiper
+                      modules={[Autoplay, EffectCreative, Navigation]}
+                      grabCursor
+                      effect={"creative"}
+                      creativeEffect={{
+                        prev: {
+                          translate: [0, 0, -400],
+                          opacity: 0,
+                        },
+                        next: {
+                          translate: ["100%", 0, 0],
+                        },
+                      }}
+                      speed={2000}
+                      loop={true}
+                      spaceBetween={10}
+                      slidesPerView={1}
+                      autoplay={{
+                        delay: 5000,
+                        // reverseDirection: true,
+                        disableOnInteraction: false,
+                        pauseOnMouseEnter: false,
+                      }}
+                      className="mySwiper -mt-[13%]"
+                    >
+                      {images.map((image, i) => (
+                        <SwiperSlide key={i}>
+                          <div className="relative mx-auto mt-[8%] w-[232px] sm:w-[288px] md:w-[344px] lg:w-[400px] xl:w-[455px] 2xl:w-[512px]">
+                            <Image
+                              src={image}
+                              alt={category}
+                              className="relative z-[1] object-contain"
+                            />
+                          </div>
+                        </SwiperSlide>
+                      ))}
+                      <SwiperNavButtons />
+                    </Swiper>
+
+                    {/* Desc */}
+                    <p className="relative -mt-[2em] me-[1.2em] ms-auto max-w-[38ch] border-e-[0.4em] border-e-primary pe-[0.4em] pt-[2em] text-right text-[11px] leading-tight sm:me-[1.4em] sm:text-[12px] md:me-[1.6em] md:max-w-[46ch] md:text-[12.5px] lg:me-[1.8em] lg:text-[13px] xl:me-[2em] xl:text-[13.5px] 2xl:me-[2em] 2xl:border-e-[0.6em] 2xl:text-[14px]">
+                      <span className="absolute bottom-[60%] right-[20%] text-[60px] font-bold leading-none text-black/20 sm:text-[78px] md:text-[96px] lg:text-[114px] xl:text-[132px] 2xl:text-[150px]">
+                        {id}
+                      </span>
+                      <span className="text-[1.8em] font-semibold">
+                        {title}
+                      </span>{" "}
+                      <br />
+                      <span className="font-bold">{emphasis}</span> {desc}
+                    </p>
+                  </article>
+                </div>
+              </SwiperSlide>
+            ),
+          )}
         </Swiper>
       </motion.div>
     </section>
