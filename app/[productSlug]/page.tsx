7ee4aa75d891c9d40fbd9bd375da8e71d1e5af76
Dominic Brian <dominic91960@gmail.com>
2025-03-01 00:06:38 +0530

    feat(products): add more product data

--- app/[productSlug]/page.tsx
+++ app/[productSlug]/page.tsx
@@ -8,123 +8,15 @@ import ProductDescription from "@/components/product-page/ProductDescription";
 import ProductCarousel from "@/components/product-page/ProductCarousel";
 import TechnicalData from "@/components/product-page/TechnicalData";
 
-import commodoreMainImg from "@/assets/images/product-page/commodore/main-img.png";
-import commodoreCardImageOne from "@/assets/images/product-page/commodore/card-img-one.png";
-import commodoreCardImageTwo from "@/assets/images/product-page/commodore/card-img-two.png";
-import commodoreCardImageThree from "@/assets/images/product-page/commodore/card-img-three.png";
-import commodoreProdDescImg from "@/assets/images/product-page/commodore/prod-desc-img.png";
-import commodoreSlideImgOne from "@/assets/images/product-page/commodore/slider-image-one.png";
-import commodoreSlideImgTwo from "@/assets/images/product-page/commodore/slider-image-two.png";
-import commodoreSlideImgThree from "@/assets/images/product-page/commodore/slider-image-three.png";
-import commodoreSlideImgFour from "@/assets/images/product-page/commodore/slider-image-four.png";
-import commodoreTechnicalData from "@/assets/images/product-page/commodore/technical-data.png";
-
-const data = [
-  {
-    subtitle: "Residential boilers",
-    mainDescOne:
-      "The SAINT ROCH Ultra is a medium power boiler that can be equipped with a forced draught oil or gas burner.",
-    title: "Commodore",
-    mainImage: commodoreMainImg,
-    mainDescTwo:
-      "Car engine png images. Automobile Engineering, Car Mechanical Engineering, Electrical engineering,",
-    cardData: [
-      {
-        id: "cd001",
-        cardImage: commodoreCardImageOne,
-        cardDesc:
-          "The SAINT ROCH Ultra is a medium power boiler that can be equipped with a forced draught oil or gas burner.",
-      },
-      {
-        id: "cd002",
-        cardImage: commodoreCardImageTwo,
-        cardDesc:
-          "It has a cast iron body and an anti-corrosion combustion chamber.",
-      },
-      {
-        id: "cd003",
-        cardImage: commodoreCardImageThree,
-        cardDesc:
-          "The SAINT ROCH Ultra satisfies all individual and collective domestic heating needs.",
-      },
-    ],
-    prodDescOne:
-      "The SAINT ROCH ULTRA cast iron boilers can be supplied complete with burners that are for forced draught oil or gas.",
-    prodDescImg: commodoreProdDescImg,
-    prodDescTwo:
-      "It is highly recommended to supply the package boiler+burner, as the SAINT ROCH burners are specifically adapted and tested for optimal operation with SAINT ROCH boilers.",
-    carouselData: [
-      {
-        id: "slider001",
-        image: commodoreSlideImgOne,
-        desc: "SAINT ROCH Hypoeutectic grey cast iron",
-      },
-      {
-        id: "slider002",
-        image: commodoreSlideImgTwo,
-        desc: "6 model range from: 27 kw - 83 kw.",
-      },
-      {
-        id: "slider003",
-        image: commodoreSlideImgThree,
-        desc: "Efficiency: at 30% charge 90-91.1% at 100% charge 89.9-90.5%",
-      },
-      {
-        id: "slider004",
-        image: commodoreSlideImgFour,
-        desc: "Operates with forced draught oil or gas burners",
-      },
-      {
-        id: "slider005",
-        image: commodoreSlideImgOne,
-        desc: "SAINT ROCH Hypoeutectic grey cast iron",
-      },
-      {
-        id: "slider006",
-        image: commodoreSlideImgTwo,
-        desc: "6 model range from: 27 kw - 83 kw.",
-      },
-      {
-        id: "slider007",
-        image: commodoreSlideImgThree,
-        desc: "Efficiency: at 30% charge 90-91.1% at 100% charge 89.9-90.5%",
-      },
-      {
-        id: "slider008",
-        image: commodoreSlideImgFour,
-        desc: "Operates with forced draught oil or gas burners",
-      },
-      {
-        id: "slider009",
-        image: commodoreSlideImgOne,
-        desc: "SAINT ROCH Hypoeutectic grey cast iron",
-      },
-      {
-        id: "slider010",
-        image: commodoreSlideImgTwo,
-        desc: "6 model range from: 27 kw - 83 kw.",
-      },
-      {
-        id: "slider011",
-        image: commodoreSlideImgThree,
-        desc: "Efficiency: at 30% charge 90-91.1% at 100% charge 89.9-90.5%",
-      },
-      {
-        id: "slider012",
-        image: commodoreSlideImgFour,
-        desc: "Operates with forced draught oil or gas burners",
-      },
-    ],
-    technicalData: commodoreTechnicalData,
-  },
-];
+import { productData } from "@/lib/constants";
 
 type Params = Promise<{ productSlug: string }>;
 
 const ProductDetailsPage = async (props: { params: Params }) => {
   const params = await props.params;
   const product =
-    data.find((prod) => prod.title === params.productSlug) || data[0];
+    productData.find((prod) => prod.path === params.productSlug) ||
+    productData[0];
 
   return (
     <>
---
+++ lib/constants.ts
@@ -0,0 +1,405 @@
+import commodoreMainImg from "@/assets/images/product-page/commodore/main-img.png";
+import commodoreCardImageOne from "@/assets/images/product-page/commodore/card-img-one.png";
+import commodoreCardImageTwo from "@/assets/images/product-page/commodore/card-img-two.png";
+import commodoreCardImageThree from "@/assets/images/product-page/commodore/card-img-three.png";
+import commodoreProdDescImg from "@/assets/images/product-page/commodore/prod-desc-img.png";
+import commodoreSlideImgOne from "@/assets/images/product-page/commodore/slider-image-one.png";
+import commodoreSlideImgTwo from "@/assets/images/product-page/commodore/slider-image-two.png";
+import commodoreSlideImgThree from "@/assets/images/product-page/commodore/slider-image-three.png";
+import commodoreSlideImgFour from "@/assets/images/product-page/commodore/slider-image-four.png";
+import commodoreTechnicalData from "@/assets/images/product-page/commodore/technical-data.png";
+
+export const productData = [
+  {
+    path: "combi",
+    subtitle: "Residential boilers",
+    mainDescOne:
+      "The SAINT ROCH Ultra is a medium power boiler that can be equipped with a forced draught oil or gas burner.",
+    title: "Combi",
+    mainImage: commodoreMainImg,
+    mainDescTwo:
+      "Car engine png images. Automobile Engineering, Car Mechanical Engineering, Electrical engineering,",
+    cardData: [
+      {
+        id: "cd001",
+        cardImage: commodoreCardImageOne,
+        cardDesc:
+          "The SAINT ROCH Ultra is a medium power boiler that can be equipped with a forced draught oil or gas burner.",
+      },
+      {
+        id: "cd002",
+        cardImage: commodoreCardImageTwo,
+        cardDesc:
+          "It has a cast iron body and an anti-corrosion combustion chamber.",
+      },
+      {
+        id: "cd003",
+        cardImage: commodoreCardImageThree,
+        cardDesc:
+          "The SAINT ROCH Ultra satisfies all individual and collective domestic heating needs.",
+      },
+    ],
+    prodDescOne:
+      "The SAINT ROCH ULTRA cast iron boilers can be supplied complete with burners that are for forced draught oil or gas.",
+    prodDescImg: commodoreProdDescImg,
+    prodDescTwo:
+      "It is highly recommended to supply the package boiler+burner, as the SAINT ROCH burners are specifically adapted and tested for optimal operation with SAINT ROCH boilers.",
+    carouselData: [
+      {
+        id: "slider001",
+        image: commodoreSlideImgOne,
+        desc: "SAINT ROCH Hypoeutectic grey cast iron",
+      },
+      {
+        id: "slider002",
+        image: commodoreSlideImgTwo,
+        desc: "6 model range from: 27 kw - 83 kw.",
+      },
+      {
+        id: "slider003",
+        image: commodoreSlideImgThree,
+        desc: "Efficiency: at 30% charge 90-91.1% at 100% charge 89.9-90.5%",
+      },
+      {
+        id: "slider004",
+        image: commodoreSlideImgFour,
+        desc: "Operates with forced draught oil or gas burners",
+      },
+      {
+        id: "slider005",
+        image: commodoreSlideImgOne,
+        desc: "SAINT ROCH Hypoeutectic grey cast iron",
+      },
+      {
+        id: "slider006",
+        image: commodoreSlideImgTwo,
+        desc: "6 model range from: 27 kw - 83 kw.",
+      },
+      {
+        id: "slider007",
+        image: commodoreSlideImgThree,
+        desc: "Efficiency: at 30% charge 90-91.1% at 100% charge 89.9-90.5%",
+      },
+      {
+        id: "slider008",
+        image: commodoreSlideImgFour,
+        desc: "Operates with forced draught oil or gas burners",
+      },
+      {
+        id: "slider009",
+        image: commodoreSlideImgOne,
+        desc: "SAINT ROCH Hypoeutectic grey cast iron",
+      },
+      {
+        id: "slider010",
+        image: commodoreSlideImgTwo,
+        desc: "6 model range from: 27 kw - 83 kw.",
+      },
+      {
+        id: "slider011",
+        image: commodoreSlideImgThree,
+        desc: "Efficiency: at 30% charge 90-91.1% at 100% charge 89.9-90.5%",
+      },
+      {
+        id: "slider012",
+        image: commodoreSlideImgFour,
+        desc: "Operates with forced draught oil or gas burners",
+      },
+    ],
+    technicalData: commodoreTechnicalData,
+  },
+  {
+    path: "ultra",
+    subtitle: "Residential boilers",
+    mainDescOne:
+      "The SAINT ROCH Ultra is a medium power boiler that can be equipped with a forced draught oil or gas burner.",
+    title: "Ultra",
+    mainImage: commodoreMainImg,
+    mainDescTwo:
+      "Car engine png images. Automobile Engineering, Car Mechanical Engineering, Electrical engineering,",
+    cardData: [
+      {
+        id: "cd001",
+        cardImage: commodoreCardImageOne,
+        cardDesc:
+          "The SAINT ROCH Ultra is a medium power boiler that can be equipped with a forced draught oil or gas burner.",
+      },
+      {
+        id: "cd002",
+        cardImage: commodoreCardImageTwo,
+        cardDesc:
+          "It has a cast iron body and an anti-corrosion combustion chamber.",
+      },
+      {
+        id: "cd003",
+        cardImage: commodoreCardImageThree,
+        cardDesc:
+          "The SAINT ROCH Ultra satisfies all individual and collective domestic heating needs.",
+      },
+    ],
+    prodDescOne:
+      "The SAINT ROCH ULTRA cast iron boilers can be supplied complete with burners that are for forced draught oil or gas.",
+    prodDescImg: commodoreProdDescImg,
+    prodDescTwo:
+      "It is highly recommended to supply the package boiler+burner, as the SAINT ROCH burners are specifically adapted and tested for optimal operation with SAINT ROCH boilers.",
+    carouselData: [
+      {
+        id: "slider001",
+        image: commodoreSlideImgOne,
+        desc: "SAINT ROCH Hypoeutectic grey cast iron",
+      },
+      {
+        id: "slider002",
+        image: commodoreSlideImgTwo,
+        desc: "6 model range from: 27 kw - 83 kw.",
+      },
+      {
+        id: "slider003",
+        image: commodoreSlideImgThree,
+        desc: "Efficiency: at 30% charge 90-91.1% at 100% charge 89.9-90.5%",
+      },
+      {
+        id: "slider004",
+        image: commodoreSlideImgFour,
+        desc: "Operates with forced draught oil or gas burners",
+      },
+      {
+        id: "slider005",
+        image: commodoreSlideImgOne,
+        desc: "SAINT ROCH Hypoeutectic grey cast iron",
+      },
+      {
+        id: "slider006",
+        image: commodoreSlideImgTwo,
+        desc: "6 model range from: 27 kw - 83 kw.",
+      },
+      {
+        id: "slider007",
+        image: commodoreSlideImgThree,
+        desc: "Efficiency: at 30% charge 90-91.1% at 100% charge 89.9-90.5%",
+      },
+      {
+        id: "slider008",
+        image: commodoreSlideImgFour,
+        desc: "Operates with forced draught oil or gas burners",
+      },
+      {
+        id: "slider009",
+        image: commodoreSlideImgOne,
+        desc: "SAINT ROCH Hypoeutectic grey cast iron",
+      },
+      {
+        id: "slider010",
+        image: commodoreSlideImgTwo,
+        desc: "6 model range from: 27 kw - 83 kw.",
+      },
+      {
+        id: "slider011",
+        image: commodoreSlideImgThree,
+        desc: "Efficiency: at 30% charge 90-91.1% at 100% charge 89.9-90.5%",
+      },
+      {
+        id: "slider012",
+        image: commodoreSlideImgFour,
+        desc: "Operates with forced draught oil or gas burners",
+      },
+    ],
+    technicalData: commodoreTechnicalData,
+  },
+  {
+    path: "commodore",
+    subtitle: "Residential boilers",
+    mainDescOne:
+      "The SAINT ROCH Ultra is a medium power boiler that can be equipped with a forced draught oil or gas burner.",
+    title: "Commodore",
+    mainImage: commodoreMainImg,
+    mainDescTwo:
+      "Car engine png images. Automobile Engineering, Car Mechanical Engineering, Electrical engineering,",
+    cardData: [
+      {
+        id: "cd001",
+        cardImage: commodoreCardImageOne,
+        cardDesc:
+          "The SAINT ROCH Ultra is a medium power boiler that can be equipped with a forced draught oil or gas burner.",
+      },
+      {
+        id: "cd002",
+        cardImage: commodoreCardImageTwo,
+        cardDesc:
+          "It has a cast iron body and an anti-corrosion combustion chamber.",
+      },
+      {
+        id: "cd003",
+        cardImage: commodoreCardImageThree,
+        cardDesc:
+          "The SAINT ROCH Ultra satisfies all individual and collective domestic heating needs.",
+      },
+    ],
+    prodDescOne:
+      "The SAINT ROCH ULTRA cast iron boilers can be supplied complete with burners that are for forced draught oil or gas.",
+    prodDescImg: commodoreProdDescImg,
+    prodDescTwo:
+      "It is highly recommended to supply the package boiler+burner, as the SAINT ROCH burners are specifically adapted and tested for optimal operation with SAINT ROCH boilers.",
+    carouselData: [
+      {
+        id: "slider001",
+        image: commodoreSlideImgOne,
+        desc: "SAINT ROCH Hypoeutectic grey cast iron",
+      },
+      {
+        id: "slider002",
+        image: commodoreSlideImgTwo,
+        desc: "6 model range from: 27 kw - 83 kw.",
+      },
+      {
+        id: "slider003",
+        image: commodoreSlideImgThree,
+        desc: "Efficiency: at 30% charge 90-91.1% at 100% charge 89.9-90.5%",
+      },
+      {
+        id: "slider004",
+        image: commodoreSlideImgFour,
+        desc: "Operates with forced draught oil or gas burners",
+      },
+      {
+        id: "slider005",
+        image: commodoreSlideImgOne,
+        desc: "SAINT ROCH Hypoeutectic grey cast iron",
+      },
+      {
+        id: "slider006",
+        image: commodoreSlideImgTwo,
+        desc: "6 model range from: 27 kw - 83 kw.",
+      },
+      {
+        id: "slider007",
+        image: commodoreSlideImgThree,
+        desc: "Efficiency: at 30% charge 90-91.1% at 100% charge 89.9-90.5%",
+      },
+      {
+        id: "slider008",
+        image: commodoreSlideImgFour,
+        desc: "Operates with forced draught oil or gas burners",
+      },
+      {
+        id: "slider009",
+        image: commodoreSlideImgOne,
+        desc: "SAINT ROCH Hypoeutectic grey cast iron",
+      },
+      {
+        id: "slider010",
+        image: commodoreSlideImgTwo,
+        desc: "6 model range from: 27 kw - 83 kw.",
+      },
+      {
+        id: "slider011",
+        image: commodoreSlideImgThree,
+        desc: "Efficiency: at 30% charge 90-91.1% at 100% charge 89.9-90.5%",
+      },
+      {
+        id: "slider012",
+        image: commodoreSlideImgFour,
+        desc: "Operates with forced draught oil or gas burners",
+      },
+    ],
+    technicalData: commodoreTechnicalData,
+  },
+  {
+    path: "optimajor",
+    subtitle: "Residential boilers",
+    mainDescOne:
+      "The SAINT ROCH Ultra is a medium power boiler that can be equipped with a forced draught oil or gas burner.",
+    title: "Optimajor",
+    mainImage: commodoreMainImg,
+    mainDescTwo:
+      "Car engine png images. Automobile Engineering, Car Mechanical Engineering, Electrical engineering,",
+    cardData: [
+      {
+        id: "cd001",
+        cardImage: commodoreCardImageOne,
+        cardDesc:
+          "The SAINT ROCH Ultra is a medium power boiler that can be equipped with a forced draught oil or gas burner.",
+      },
+      {
+        id: "cd002",
+        cardImage: commodoreCardImageTwo,
+        cardDesc:
+          "It has a cast iron body and an anti-corrosion combustion chamber.",
+      },
+      {
+        id: "cd003",
+        cardImage: commodoreCardImageThree,
+        cardDesc:
+          "The SAINT ROCH Ultra satisfies all individual and collective domestic heating needs.",
+      },
+    ],
+    prodDescOne:
+      "The SAINT ROCH ULTRA cast iron boilers can be supplied complete with burners that are for forced draught oil or gas.",
+    prodDescImg: commodoreProdDescImg,
+    prodDescTwo:
+      "It is highly recommended to supply the package boiler+burner, as the SAINT ROCH burners are specifically adapted and tested for optimal operation with SAINT ROCH boilers.",
+    carouselData: [
+      {
+        id: "slider001",
+        image: commodoreSlideImgOne,
+        desc: "SAINT ROCH Hypoeutectic grey cast iron",
+      },
+      {
+        id: "slider002",
+        image: commodoreSlideImgTwo,
+        desc: "6 model range from: 27 kw - 83 kw.",
+      },
+      {
+        id: "slider003",
+        image: commodoreSlideImgThree,
+        desc: "Efficiency: at 30% charge 90-91.1% at 100% charge 89.9-90.5%",
+      },
+      {
+        id: "slider004",
+        image: commodoreSlideImgFour,
+        desc: "Operates with forced draught oil or gas burners",
+      },
+      {
+        id: "slider005",
+        image: commodoreSlideImgOne,
+        desc: "SAINT ROCH Hypoeutectic grey cast iron",
+      },
+      {
+        id: "slider006",
+        image: commodoreSlideImgTwo,
+        desc: "6 model range from: 27 kw - 83 kw.",
+      },
+      {
+        id: "slider007",
+        image: commodoreSlideImgThree,
+        desc: "Efficiency: at 30% charge 90-91.1% at 100% charge 89.9-90.5%",
+      },
+      {
+        id: "slider008",
+        image: commodoreSlideImgFour,
+        desc: "Operates with forced draught oil or gas burners",
+      },
+      {
+        id: "slider009",
+        image: commodoreSlideImgOne,
+        desc: "SAINT ROCH Hypoeutectic grey cast iron",
+      },
+      {
+        id: "slider010",
+        image: commodoreSlideImgTwo,
+        desc: "6 model range from: 27 kw - 83 kw.",
+      },
+      {
+        id: "slider011",
+        image: commodoreSlideImgThree,
+        desc: "Efficiency: at 30% charge 90-91.1% at 100% charge 89.9-90.5%",
+      },
+      {
+        id: "slider012",
+        image: commodoreSlideImgFour,
+        desc: "Operates with forced draught oil or gas burners",
+      },
+    ],
+    technicalData: commodoreTechnicalData,
+  },
+];
