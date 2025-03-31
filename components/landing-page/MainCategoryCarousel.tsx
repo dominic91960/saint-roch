1d1d5c0aca3b5c6b0b4b82806cceb7c5c5fcad12
Dominic Brian <dominic91960@gmail.com>
2025-03-31 05:56:25 +0530

    fix(homepage): main categories changes

--- components/landing-page/MainCategoryCarousel.tsx
+++ components/landing-page/MainCategoryCarousel.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Image, { StaticImageData } from "next/image";
 
+import { motion } from "framer-motion";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import "swiper/css";
@@ -28,17 +29,21 @@ const MainCategoryCarousel: React.FC<MainCategoryCarouselProps> = ({
     >
       {images.map(({ id, title, image, alt }) => (
         <SwiperSlide key={id}>
-          <div className="relative flex items-center justify-center pb-[50px] pt-[0.7em]">
+          <div className="relative flex flex-col items-center justify-center overflow-hidden pb-[50px] pt-[0.7em]">
+            <motion.h2
+              initial={{ opacity: 0, y: 100 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ amount: 0.8 }}
+              transition={{ duration: 1 }}
+              className={`light-text-stroke uppercase leading-none ${title === "Optimajor" || title === "Commodore" ? "top-[4%] text-[0.85em] sm:text-[0.83em] md:text-[0.8em] 2xl:text-[0.94em]" : "top-0 text-[1.75em]"}`}
+            >
+              {title}
+            </motion.h2>
             <Image
               src={image}
               alt={alt}
-              className="relative z-[1] w-full object-contain"
+              className="relative z-[1] -mt-[5%] w-full object-contain"
             />
-            <h2
-              className={`light-text-stroke absolute left-0 uppercase leading-none ${title === "Optimajor" || title === "Commodore" ? "top-[4%] text-[0.85em] sm:text-[0.83em] md:text-[0.8em] 2xl:text-[0.94em]" : "top-0 text-[1.75em]"}`}
-            >
-              {title}
-            </h2>
           </div>
         </SwiperSlide>
       ))}
